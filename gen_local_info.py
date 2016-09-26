43a42dbf173b7829bc5311be61286070a64c58dc
LIT <LITTOMA@163.com>
2016-09-26 18:09:50 +0800

    Create gen_local_info.py

---
+++ gen_local_info.py
@@ -0,0 +1,34 @@
+import urllib2,re,os,traceback
+
+def mkdir(path):
+	if not os.path.exists(path):
+		print 'make dir:', path
+		os.makedirs(path)
+
+url = 'http://3dsdb.com/xml.php'
+xml = urllib2.urlopen(url).read()
+
+#xml = open('3dsreleases.xml','r').read()
+
+partten = r'<release>[\s|\S]+?</release>'
+partten = re.compile(partten)
+matches = partten.findall(xml)
+
+tidpar = re.compile(r'<titleid>(.+?)</titleid>')
+regpar = re.compile(r'<region>(.+?)</region>')
+langpar = re.compile(r'<languages>(.+?)</languages>')
+for m in matches:
+	try:
+		tid = tidpar.search(m).group(1)
+		region = regpar.search(m).group(1)
+		language = langpar.search(m).group(1).split(',')[0]
+		mkdir('luma/locales/')
+		filepath = 'luma/locales/%s.txt'%tid
+		if os.path.exists(filepath):
+			print 'File: "%s" exists, skip'%filepath
+			continue
+		with open('luma/locales/%s.txt'%tid, 'w')as txt:
+			txt.write('%s %s'%(region.upper(), language.upper()))
+	except:
+		traceback.print_exc()
+		continue
